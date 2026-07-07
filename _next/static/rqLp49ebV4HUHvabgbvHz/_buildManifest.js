6c829818887079055c8f54f04c3d3661552a9917
AbdullahChishti <AbdullahChishti@users.noreply.github.com>
2026-07-07 22:44:06 +0000

    deploy: 1a8b6a4f175128dadc5bf3e24c773acc1f9feec0

---
+++ _next/static/rqLp49ebV4HUHvabgbvHz/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/runtimeStudio//_next/:path+",
+        "destination": "/runtimeStudio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
